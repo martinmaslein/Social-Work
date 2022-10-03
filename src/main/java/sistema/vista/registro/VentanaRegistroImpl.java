522bb7e9b71ffc35b23884812993581a13f0bc16
QuimeyRodi <quimeyrodi@gmail.com>
2022-10-02 21:23:39 -0300

    comboPlan

--- src/main/java/sistema/vista/registro/VentanaRegistroImpl.java
+++ src/main/java/sistema/vista/registro/VentanaRegistroImpl.java
@@ -1,36 +1,27 @@
 package sistema.vista.registro;
 
 import java.awt.Dimension;
-
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
-
-import sistema.controlador.ControladorCliente;
-import sistema.vista.cliente.VentanaCliente;
 import sistema.vista.login.VentanaLogin;
 import sistema.vista.login.VentanaLoginImpl;
-
 import javax.swing.JTextField;
-import javax.swing.text.AttributeSet.ColorAttribute;
-
-import java.awt.BorderLayout;
-import java.awt.Color;
 import javax.swing.JLabel;
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import javax.swing.ImageIcon;
+import javax.swing.JComboBox;
 
 public class VentanaRegistroImpl extends JFrame implements VentanaRegistro {
 
 	public VentanaRegistroImpl(VentanaLoginImpl login) {
-		setSize (new Dimension(700,700));
+		setSize(new Dimension(700, 700));
 		this.inicializar();
 		this.login = login;
-		
 	}
-	
+
 	public void mostrarVentana(boolean m) throws Exception {
 		this.setVisible(m);
 	}
@@ -38,12 +29,11 @@ public class VentanaRegistroImpl extends JFrame implements VentanaRegistro {
 	public void eliminarVentana() {
 		this.dispose();
 	}
-		
+
 	public void informar(String mensaje) {
 		JOptionPane.showMessageDialog(null, mensaje);
-		
 	}
-	
+
 	protected JPanel PanelRegistro;
 	protected VentanaLogin login;
 	protected JTextField campoNombre;
@@ -54,59 +44,55 @@ public class VentanaRegistroImpl extends JFrame implements VentanaRegistro {
 	protected JTextField campoDireccion;
 	protected JTextField campoCorreo;
 	protected JTextField campoTelefono;
-
 	protected JTextField campoContrasena;
-	
-	
-	
+
 	private void inicializar() {
 		this.setTitle("Registrarse");
 		this.setResizable(false);
-		this.setBounds(100, 100, 700,600);
+		this.setBounds(100, 100, 700, 600);
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		
+
 		this.PanelRegistro = new JPanel();
 		PanelRegistro.setLayout(null);
 		this.PanelRegistro.setPreferredSize(new Dimension(550, 500));
 		this.PanelRegistro.setVisible(true);
-		
-			
+
 		campoNombre = new JTextField();
-		campoNombre.setBounds(146, 57, 295, 26);
+		campoNombre.setBounds(146, 40, 295, 26);
 		campoNombre.setPreferredSize(new Dimension(70, 50));
-		
+
 		campoApellido = new JTextField();
-		campoApellido.setBounds(146, 94, 295, 26);
+		campoApellido.setBounds(146, 77, 295, 26);
 		campoApellido.setPreferredSize(new Dimension(70, 50));
-		
+
 		campoNombreDeUsuario = new JTextField();
-		campoNombreDeUsuario.setBounds(146, 354, 295, 26);
+		campoNombreDeUsuario.setBounds(146, 374, 295, 26);
 		campoNombreDeUsuario.setPreferredSize(new Dimension(70, 50));
-		
+
 		campoDni = new JTextField();
-		campoDni.setBounds(146, 131, 295, 26);
+		campoDni.setBounds(146, 114, 295, 26);
 		campoDni.setPreferredSize(new Dimension(70, 50));
-		
+
 		campoDireccion = new JTextField();
-		campoDireccion.setBounds(146, 168, 295, 26);
+		campoDireccion.setBounds(146, 151, 295, 26);
 		campoDireccion.setPreferredSize(new Dimension(70, 50));
-		
+
 		campoCorreo = new JTextField();
-		campoCorreo.setBounds(146, 242, 295, 26);
+		campoCorreo.setBounds(146, 225, 295, 26);
 		campoCorreo.setPreferredSize(new Dimension(70, 50));
-		
+
 		campoFechaNacimiento = new JTextField();
-		campoFechaNacimiento.setBounds(146, 205, 295, 26);
+		campoFechaNacimiento.setBounds(146, 188, 295, 26);
 		campoFechaNacimiento.setPreferredSize(new Dimension(70, 50));
-		
+
 		campoTelefono = new JTextField();
-		campoTelefono.setBounds(146, 279, 295, 26);
+		campoTelefono.setBounds(146, 262, 295, 26);
 		campoTelefono.setPreferredSize(new Dimension(70, 50));
-		
+
 		campoContrasena = new JTextField();
-		campoContrasena.setBounds(146, 391, 295, 26);
+		campoContrasena.setBounds(146, 411, 295, 26);
 		campoContrasena.setPreferredSize(new Dimension(70, 50));
-		
+
 		this.PanelRegistro.add(campoNombre);
 		this.PanelRegistro.add(campoApellido);
 		this.PanelRegistro.add(campoDni);
@@ -116,49 +102,49 @@ public class VentanaRegistroImpl extends JFrame implements VentanaRegistro {
 		this.PanelRegistro.add(campoNombreDeUsuario);
 		this.PanelRegistro.add(campoTelefono);
 		this.PanelRegistro.add(campoContrasena);
-		
+
 		this.setContentPane(this.PanelRegistro);
-		
+
 		JLabel textoNombre = new JLabel("Nombre");
-		textoNombre.setBounds(60, 60, 73, 20);
+		textoNombre.setBounds(60, 43, 73, 20);
 		PanelRegistro.add(textoNombre);
-		
+
 		JLabel textoApellido = new JLabel("Apellido");
-		textoApellido.setBounds(60, 100, 46, 14);
+		textoApellido.setBounds(60, 83, 46, 14);
 		PanelRegistro.add(textoApellido);
-		
+
 		JLabel textoDni = new JLabel("DNI");
-		textoDni.setBounds(60, 135, 46, 14);
+		textoDni.setBounds(60, 120, 46, 14);
 		PanelRegistro.add(textoDni);
-		
+
 		JLabel textoDireccion = new JLabel("Direccion");
-		textoDireccion.setBounds(60, 172, 59, 14);
+		textoDireccion.setBounds(60, 157, 59, 14);
 		PanelRegistro.add(textoDireccion);
-		
+
 		JLabel textoFechaDeNacimiento = new JLabel("Fecha de nac");
-		textoFechaDeNacimiento.setBounds(60, 211, 113, 14);
+		textoFechaDeNacimiento.setBounds(60, 194, 113, 14);
 		PanelRegistro.add(textoFechaDeNacimiento);
-		
+
 		JLabel lblCorreo = new JLabel("Correo");
-		lblCorreo.setBounds(60, 246, 46, 14);
+		lblCorreo.setBounds(60, 231, 46, 14);
 		PanelRegistro.add(lblCorreo);
-		
+
 		JLabel lblTelefono = new JLabel("Telefono");
-		lblTelefono.setBounds(60, 283, 73, 14);
+		lblTelefono.setBounds(60, 268, 73, 14);
 		PanelRegistro.add(lblTelefono);
-		
+
 		JLabel lblUsuario = new JLabel("Usuario");
-		lblUsuario.setBounds(60, 360, 46, 14);
+		lblUsuario.setBounds(60, 380, 46, 14);
 		PanelRegistro.add(lblUsuario);
-		
+
 		JLabel lblContrasenia = new JLabel("Contraseña");
-		lblContrasenia.setBounds(60, 397, 76, 14);
+		lblContrasenia.setBounds(60, 417, 76, 14);
 		PanelRegistro.add(lblContrasenia);
-		
+
 		JButton btnConfirmar = new JButton("Confirmar");
-		btnConfirmar.setBounds(223, 446, 102, 26);
+		btnConfirmar.setBounds(426, 463, 102, 26);
 		PanelRegistro.add(btnConfirmar);
-		
+
 		JButton btnVolver = new JButton("");
 		btnVolver.setIcon(new ImageIcon("img\\flechi.png"));
 		btnVolver.addActionListener(new ActionListener() {
@@ -167,13 +153,22 @@ public class VentanaRegistroImpl extends JFrame implements VentanaRegistro {
 				try {
 					login.mostrarVentana(true);
 				} catch (Exception e1) {
-					// TODO Auto-generated catch block
 					e1.printStackTrace();
 				}
 			}
 		});
 		btnVolver.setBounds(10, 11, 35, 31);
 		PanelRegistro.add(btnVolver);
+
+		JComboBox<String> comboPlan = new JComboBox();
+		comboPlan.setBounds(146, 299, 46, 22);
+		PanelRegistro.add(comboPlan);
+		comboPlan.addItem("A");
+		comboPlan.addItem("B");
+
+		JLabel lblPlan = new JLabel("Plan");
+		lblPlan.setBounds(60, 304, 73, 14);
+		PanelRegistro.add(lblPlan);
 		this.pack();
 		this.setVisible(true);
 	}
